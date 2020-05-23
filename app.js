eb1a34ef2e4105cf84dbc16389d15bdecf4a8282
Faeshal <faeshal111@gmail.com>
2020-05-23 16:17:47 +0700

    second try

--- app.js
+++ app.js
@@ -101,33 +101,3 @@ app.listen(PORT, (err) => {
     console.log(chalk.black.bgGreen(`Server is Running On Port : ${PORT}`));
   }
 });
-
-// // * Heathcheck TINI
-// // quit on ctrl-c when running docker in terminal
-// process.on("SIGINT", function onSigint() {
-//   console.info(
-//     "Got SIGINT (aka ctrl-c in docker). Graceful shutdown ",
-//     new Date().toISOString()
-//   );
-//   shutdown();
-// });
-
-// // quit properly on docker stop
-// process.on("SIGTERM", function onSigterm() {
-//   console.info(
-//     "Got SIGTERM (docker container stop). Graceful shutdown ",
-//     new Date().toISOString()
-//   );
-//   shutdown();
-// });
-
-// // shut down server
-// function shutdown() {
-//   server.close(function onServerClosed(err) {
-//     if (err) {
-//       console.error(err);
-//       process.exitCode = 1;
-//     }
-//     process.exit();
-//   });
-// }
